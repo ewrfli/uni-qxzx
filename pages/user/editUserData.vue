6cfb288e208f15f734a53a5849b7c30e38f94af2
ewrfli <q304062@163.com>
2022-02-08 15:05:52 +0800

    feat: add userdata edit

--- pages/user/editUserData.vue
+++ pages/user/editUserData.vue
@@ -1,21 +1,41 @@
 <template>
 	<view class="wrap">
 		<u-navbar title="编辑资料" @leftClick="leftClick" :autoBack="true" :fixed="true">
-			<view
-                class="u-nav-slot"
-                slot="left"
-            >
-                <u-icon
-                    name="arrow-left"
-                    size="22"
-					color="#007aff"
-                ></u-icon>
-
+			<view class="u-nav-slot" slot="left">
+                <u-icon name="arrow-left" size="22" color="#007aff"></u-icon>
             </view>
-		
+			<view class="u-nav-slot" slot="right">
+                <u-button type="primary" text="保存"></u-button>
+            </view>
 		</u-navbar>
-		<view class="card">	
-
+		<view class="top-card">	
+			<view class="avatar">
+				<image :src="userData.userImg" mode=""></image>
+			</view>
+			<view class="right-div">
+				<view class="name">用户111</view>
+				<view class="accountnum">账号：11111111</view>
+				<view class="perfect">资料完善度</view>
+			</view>
+		</view>
+		<view class="mid-card">
+			<view class="mid-title">基本信息</view>
+            <u-line length="100%" style="margin: 0 auto;"></u-line>
+			<view class="item">
+				<view class="title">介绍</view>
+				<view class="desc">xxxxxxxx</view>
+			</view>
+			<u-line length="100%" style="margin: 0 auto;"></u-line> 
+			<view class="item">
+				<view class="title">性别</view>
+				<view class="desc"><input v-model="userData.sex"/></view>
+			</view>
+			<u-line length="100%" style="margin: 0 auto;"></u-line>
+			<view class="item">
+				<view class="title">公司</view>
+				<view class="desc">xxxxxxxx公司</view>
+			</view>
+			<u-line length="100%" style="margin: 0 auto;"></u-line>
 		</view>
 	</view>
 </template>
@@ -25,7 +45,21 @@
 		name:"newsHotDetails",
 		data() {
 			return {
-
+				userData: {
+					sex: '男',
+					id: '',
+					userImg: 'https://img.36krcdn.com/20200410/v2_6905947498bc4ec0af228afed409f771_img_png',
+					userName: '杨洋样',
+					time: 2,
+					readNum: 2233,
+					contentTitle: '自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶自动驾驶',
+					contentText: 'XXXXXXXXXXXXXXXX',
+					relatedCompany: '自动驾驶',
+					repostNum: 11,
+					commentNum: 22,
+					likeNum: 99,
+					icon: ''
+				}
 			};
 		},
 		methods: {
@@ -41,40 +75,64 @@
 </script>
 
 <style lang="scss">
-	.content{
+	.wrap{
 		background-color: $uni-color-backgroundColor;
 	}
-	.card {
-		margin: 50px 6px 0;
-		padding: 0px 10px 0px 10px;
-		background-color: $uni-color-content;
-		.u-navbar__content {
-			background-color: aqua !important;
+	.u-nav-slot {
+		.u-button{
+			height: 28px;
 		}
-		.itemList {
-			background-color: $uni-color-content;
+	}
+	.top-card {
+		background-color: #ffffff;
+		padding: 0 15px;
+		margin-top: 44px;
+		display: flex;
+		.avatar {
+			padding: 10px;
+			margin-top: 2px;
+			image {
+				width: 78px;
+				height: 78px;
+				border-radius: 39px;
+			}
 		}
-		.item {
-			// background-color: antiquewhite;
-			border-bottom: 0.5px solid $uni-border-color;
-			height: 36px;
-			line-height: 28px;
-			margin-top: 10px;
+		.right-div {
+			padding: 10px;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			.name {
+				font-size: large;
+				font-weight: 600;
+			}
+			.accountnum {
+				color: #afafaf;
+                font-size: 16px;
+			}
+			.perfect {
+				color: #afafaf;
+                font-size: 16px;
+			}
+		}
+	}
+	.mid-card {
+		margin-top: 10px;
+		background-color: #ffffff;
+        margin-top: 10px;
+        .mid-title {
+            padding: 10px 20px;
+			font-weight: 600;
+        }
+		.item{
 			display: flex;
 			flex-direction: row;
-			justify-content: flex-start;
-
-			.left-icon {
-				.u-icon{
-					margin-top: 6px;
-				}
-			}
+			padding: 10px 20px;
 			.title {
-				margin-left: 8px;
-				.Content-title {
-					font-size: 16px;
-				}
-
+				width: 80px;
+			}
+			.desc {
+				color: #afafaf;
 			}
 		}
 	}
